53aecef84f75a6ede49ee8335cc2db09418fcf0b
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-29 16:13:32 +0800

    67eutyjtyj

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,19 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use Faker\Generator as Faker;
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+
+$factory->define(App\User::class, function (Faker $faker) {
     return [
+        'phone' => $faker->numberBetween(10000000000, 19999999999),
+        'password' => '$2y$10$sr6a1PMteAvWvGZMdmhTPu7m3G9SAj/2uBRovZ3ZiEDwsVPtxs3Vu', //123123
         'name' => $faker->name,
-        'email' => $faker->email,
+        'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(App\Contact::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->randomNumber(),
+        'phone' => $faker->numberBetween(10000000000, 19999999999),
+        'name' => $faker->name,
+        'address' => $faker->address,
+        'default' => $faker->boolean,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 62)->create()->each(function ($u) {
+            $u->contacts()->save(factory(App\Contact::class)->make());
+        });
+
+        // factory(App\User::class, 60)->create();
+    }
+}
