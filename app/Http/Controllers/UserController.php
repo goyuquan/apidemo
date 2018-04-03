f149cc1b364c381929a9d45aa9d4fe0ba9bebbf1
unknown <guoyuquan@QL-01-0591.ql.com>
2018-04-03 09:50:06 +0800

    ewgrewgreg

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
   */
   public function index()
   {
-    return Carbon::now();
+    return Carbon::now()->local;
     return response(['data' => User::all()->toArray()]);
   }
   /**
