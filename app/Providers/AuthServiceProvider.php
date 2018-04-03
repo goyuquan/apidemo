28ee40556bf6c183608b1790a84c03ef11c0b20b
unknown <guoyuquan@QL-01-0591.ql.com>
2018-04-03 14:47:36 +0800

    eqtreahfdshf

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,15 +26,15 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app['auth']->viaRequest('api', function ($request) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ], 401);
             if ($request->header('Authorization')) {
                 $key = $request->header('Authorization');
                 $user = User::where('remember_token', $key)->first();
                 $user->update('updated_at', Carbon::now()->toDateTimeString());
 
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Unauthorized'
-                ], 401);
                 if(!empty($user)) {
                     $request->request->add(['userid' => $user->id]);
                     return $user;
