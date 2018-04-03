ac2fb536f2f5ad58e454eba03f7766bcb3692377
unknown <guoyuquan@QL-01-0591.ql.com>
2018-04-03 10:32:19 +0800

    ehweryerwyt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,8 @@ use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Carbon;
-/**
-* Class UserController
-*/
+
+
 class UserController extends Controller
 {
   /**
@@ -17,7 +16,6 @@ class UserController extends Controller
   */
   public function index()
   {
-    return Carbon::now()->toDateTimeString();   
     return response(['data' => User::all()->toArray()]);
   }
   /**
@@ -30,132 +28,112 @@ class UserController extends Controller
   public function store(Request $request)
   {
     $this->validate($request, [
-        'name' => 'required',
-        'email' => 'required|unique:users|email',
-        'password' => 'required'
-      ]
-    );
+      'name' => 'required',
+      'phone' => 'required|unique:users|phone',
+      'password' => 'required'
+    ]
+  );
 
-    $data = [
-       'name' => $request->input('name'),
-      'email' => $request->input('email'),
-      'password' => Hash::make($request->input('password'))
-    ];
+  $data = [
+    'name' => $request->input('name'),
+    'phone' => $request->input('phone'),
+    'password' => Hash::make($request->input('password'))
+  ];
 
-    $user = User::create($data);
-    $statusCode = $user ? 200 : 422;
-    return response(
-      [
-        'data' => $user,
-        'status' => $user ? "success" : "error",
-      ], $statusCode
-    );
+  $user = User::create($data);
+  $statusCode = $user ? 200 : 422;
+  return response(
+    [
+      'data' => $user,
+      'status' => $user ? "success" : "error",
+    ], $statusCode
+  );
+}
+
+
+public function show($userId)
+{
+  try {
+    $user = User::findOrFail($userId);
+  } catch (\Exception $e) {
+    $user = null;
+    $statusCode = 404;
   }
-  /**
-  * Get one user from the database
-  *
-  * @param int $userId userId
-  *
-  * @return \Illuminate\Http\JsonResponse
-  */
-  public function show($userId)
+  return response(
+    [
+      'data' => $user,
+      'status' => $user ? "success" : "Not found.",
+    ], $statusCode ?? 201
+  );
+}
+
+
+public function update(Request $request, $userId)
+{
+  try {
+    $user = self::userExist($userId);
+    $user->update($request->only('name', 'password'));
+  } catch(\Exception $e) {
+    $user = null;
+    $statusCode = 404;
+  }
+  return response(
+    [
+      "data" => $user,
+      "status" => $user ? "success" : "Not found."
+    ], $statusCode ?? 200
+  );
+}
+
+
+public function delete($userId)
+{
+  try {
+    $user = self::userExist($userId);
+    $user->delete();
+  } catch(\Exception $e) {
+    $user = null;
+    $statusCode = 404;
+  }
+  return response(
+    [
+      "data" => $user,
+      "status" => $user ? "success" : "Not found."
+    ], $statusCode ?? 200
+  );
+}
+
+
+protected static function userExist($id)
+{
+  return User::findOrFail($id);
+}
+
+
+public function authenticate(Request $request)
+{
+  $this->validate($request, [
+    'phone' => 'required',
+    'password' => 'required'
+  ]);
+
+  $user = User::where('phone', $request->input('phone'))->first();
+
+  if(Hash::check($request->input('password'), $user->password)){
+    $apikey = base64_encode(str_random(40));
+    User::where('phone', $request->input('phone'))->update(['api_key' => "$apikey"]);
+    return response()->json(['status' => 'success','api_key' => $apikey, 'data' => $user]);
+  }else{
+    return response()->json(['status' => 'fail'],401);
+  }
+}
+
+
+public function logout($id)
+{
+  if(User::where('id', $id)->update(['api_key' => '']))
   {
-    try {
-      $user = User::findOrFail($userId);
-    } catch (\Exception $e) {
-      $user = null;
-      $statusCode = 404;
-    }
-    return response(
-      [
-        'data' => $user,
-        'status' => $user ? "success" : "Not found.",
-      ], $statusCode ?? 201
-    );
+    return response()->json(['status' => 'success'], 200);
   }
-  /**
-  * Implement a full/partial update
-  *
-  * @param Request $request request
-  * @param int     $userId  userId
-  *
-  * @return \Illuminate\Http\JsonResponse
-  */
-  public function update(Request $request, $userId)
-  {
-    try {
-      $user = self::userExist($userId);
-      $user->update($request->only('name', 'password'));
-    } catch(\Exception $e) {
-      $user = null;
-      $statusCode = 404;
-    }
-    return response(
-      [
-        "data" => $user,
-        "status" => $user ? "success" : "Not found."
-      ], $statusCode ?? 200
-    );
-  }
-  /**
-  * Delete a resource
-  *
-  * @param int $userId userId
-  *
-  * @return \Illuminate\Http\JsonResponse
-  */
-  public function delete($userId)
-  {
-    try {
-      $user = self::userExist($userId);
-      $user->delete();
-    } catch(\Exception $e) {
-      $user = null;
-      $statusCode = 404;
-    }
-    return response(
-      [
-        "data" => $user,
-        "status" => $user ? "success" : "Not found."
-      ], $statusCode ?? 200
-    );
-  }
-  /**
-  * Check if user exist by id
-  *
-  * @param int $id id
-  *
-  * @return bool|mixed|static
-  */
-  protected static function userExist($id)
-  {
-      return User::findOrFail($id);
-  }
-
-
-    public function authenticate(Request $request)
-    {
-       $this->validate($request, [
-           'phone' => 'required',
-           'password' => 'required'
-        ]);
-
-        $user = User::where('email', $request->input('email'))->first();
-
-        if(Hash::check($request->input('password'), $user->password)){
-            $apikey = base64_encode(str_random(40));
-            User::where('email', $request->input('email'))->update(['api_key' => "$apikey"]);
-            return response()->json(['status' => 'success','api_key' => $apikey, 'data' => $user]);
-        }else{
-            return response()->json(['status' => 'fail'],401);
-        }
-    }
-
-  public function logout($id)
-    {
-        if(User::where('id', $id)->update(['api_key' => '']))
-      	{
-      		  return response()->json(['status' => 'success'], 200);
-      	}
-    }
+}
 }
