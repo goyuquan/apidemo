0756b5bb1b3134a5ebe72d42cf2465e8e359229d
unknown <guoyuquan@QL-01-0591.ql.com>
2018-04-03 10:03:29 +0800

    wetrhryyh

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
   */
   public function index()
   {
-    // return new DateTime('now');
+    return Carbon::getTestNow(); 
     return response(['data' => User::all()->toArray()]);
   }
   /**
