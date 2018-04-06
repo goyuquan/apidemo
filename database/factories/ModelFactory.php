32f9d0e4e327d4f6ab384909838cb94cc0a1b45f
goyuquan <gyqzone@sina.com>
2018-04-06 15:45:04 +0800

    fgasdfgfg

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -5,11 +5,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'contact_id' => $faker->randomNumber(2),
+        'contact_id' => $faker->randomNumber(1),
         'phone' => $faker->numberBetween(10000000000, 19999999999),
         'password' => '$2y$10$sr6a1PMteAvWvGZMdmhTPu7m3G9SAj/2uBRovZ3ZiEDwsVPtxs3Vu', //123123
         'name' => $faker->name,
-        'role' => $faker->randomNumber(2),
+        'role' => $faker->randomNumber(1),
     ];
 });
 
