cbad90cdd0c5bc4b27ae8b7151d1f11b605f00fb
goyuquan <gyqzone@sina.com>
2018-04-07 13:27:49 +0800

    sadfsadf

--- database/migrations/2018_04_06_111305_create_order_records_table.php
+++ database/migrations/2018_04_06_111305_create_order_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderRecordTable extends Migration
+class CreateOrderRecordsTable extends Migration
 {
     /**
      * Run the migrations.
