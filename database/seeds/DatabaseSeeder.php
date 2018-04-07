b18bb4f50e541cbdeae6ca918a30a879532ecfbe
goyuquan <gyqzone@sina.com>
2018-04-07 12:57:10 +0800

    sdfsdf

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             // ContactsTableSeeder::class,
-            Shopping_cartTableSeeder::class,
+            // Shopping_cartTableSeeder::class,
         ]);
     }
 }
