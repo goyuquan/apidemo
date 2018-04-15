d21de6325a7cccc6f254a5846b8c03667f9a1943
goyuquan <gyqzone@sina.com>
2018-04-15 15:59:40 +0800

    sdfsdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Product;
 use Validator;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Carbon;
