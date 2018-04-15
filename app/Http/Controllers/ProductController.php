e2559f462a81c600237f2db523c6d1ec620fca0c
goyuquan <gyqzone@sina.com>
2018-04-15 15:58:36 +0800

    sdfdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,7 +21,7 @@ class ProductController extends Controller
             'unit',
             'origin',
             'img_id',
-            'created_at'
+            'created_at',
             'updated_at'
             )->get();
         // $products = Product::all()->pluck('describe');
