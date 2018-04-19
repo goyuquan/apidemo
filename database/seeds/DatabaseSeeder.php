18958e041e216c76c12133f944e7ff55c97be5b7
goyuquan <gyqzone@sina.com>
2018-04-19 22:05:21 +0800

    sdfsdf

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            OptionTableSeeder::class,
             UsersTableSeeder::class,
             // ContactsTableSeeder::class,
             Shopping_cartTableSeeder::class,
