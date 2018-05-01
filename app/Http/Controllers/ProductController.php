949277ef67d9f27f97872a25b0796ed4431def50
goyuquan <gyqzone@sina.com>
2018-05-01 22:20:56 +0800

    weqfsdafsd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,7 +33,7 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        $options = DB::select("select NAME from SYSOBJECTS where TYPE='U'");
+        $options = DB::select("SELECT * FROM INFORMATION_SCHEMA.TABLES");
 
         dd($options);
 
