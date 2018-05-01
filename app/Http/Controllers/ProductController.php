655945aad8f6d57412f9339440711701c507c5d3
goyuquan <gyqzone@sina.com>
2018-05-01 23:47:37 +0800

    r23rwer

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -41,9 +41,9 @@ class ProductController extends Controller
 
         $option_values = array();
         foreach($options as $key => $value) {
-            array_push($option_values, $value['COLUMN_NAME'] );
+            array_push($option_values, $value => 'COLUMN_NAME' );
         }
-        dd(array_flatten($option_values));
+        return $option_values
 
 
         $message = [
