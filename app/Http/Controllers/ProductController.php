00d795daef2cbd9edf421c8a89837d44af1da785
goyuquan <gyqzone@sina.com>
2018-05-01 22:31:24 +0800

    sdfsdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,7 +33,7 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        $options = DB::select("SELECT distinct * FROM information_schema.column");
+        $options = DB::select("SELECT distinct * FROM information_schema.columns");
 
         dd($options);
 
